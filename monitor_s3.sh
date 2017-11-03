981e2d73f91dc12477d359d6e3d700aec9b63d0a
epa <epa@localhost.localdomain>
2017-11-03 15:44:15 +0000

    new update

--- monitor_s3.sh
+++ monitor_s3.sh
@@ -1,14 +1,8 @@
 #!/bin/bash
 
-aws s3 ls s3://epa-lab-week7/NasdaqTickerSymbolList.csv
+cnt=`grep aws s3 ls s3://epa-lab-week7/NasdaqTickerSymbolList.csv | wc -l`
 
-cnt=`grep enabled NasdaqTickerSymbolList.csv | wc -l`
-
- if [ $cnt -gt 0 ]; then 
-
- echo "1 files found in bucket"
-
- else
+ if [ $cnt -le 2 ]; then 
 
  echo "0 files found in bucket"
 
