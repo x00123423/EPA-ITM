d526087be1103897feac7c26da9241366ff574d6
epa <epa@localhost.localdomain>
2017-11-16 10:28:33 +0000

    foreground

--- week8/loadWrapper.sh
+++ week8/loadWrapper.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
-./loadtest $1
+./loadtest $1 & 
+# is to backgroud loadtest
+# process and detach it from the shell
+
+# fg will retrun the backrounded process
+# to the  foreground
 
 sleep  $2
 
 # kills the name  of the process
 pkill  loadtest 
 
-fi
